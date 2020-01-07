93cacbf06901c0bb45ee7e21add62beac174669d
José Ramos Carvalho Neto <joseneto.estag@trt16.jus.br>
2020-01-07 12:41:21 -0300

    Fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,43 +1,60 @@
 class HttpService{
+    _handlerErros(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url){
-        return new Promise((resolve,reject) => {
+        return fetch(url)
+                .then(res => this._handlerErros(res))
+                .then(res => res.json());
 
-            let xhr = new XMLHttpRequest();
+        // return new Promise((resolve,reject) => {
 
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
+        //     let xhr = new XMLHttpRequest();
+
+        //     xhr.open('GET', url);
+        //     xhr.onreadystatechange = () => {
+        //         if(xhr.readyState == 4){
+        //             if(xhr.status == 200){
                         
-                        resolve(JSON.parse(xhr.responseText));
+        //                 resolve(JSON.parse(xhr.responseText));
                         
-                    }else{
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
+        //             }else{
+        //                 console.log(xhr.responseText);
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     }
 
-            xhr.send(); 
-        });
+        //     xhr.send(); 
+        // });
     }
 
     post(url,dado){
-        return new Promise((resolve,reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-Type","application/json");
 
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        resolve(JSON.parse(xhr.responseText));
-                    }else{
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url,{
+                    headers: {'Content-Type': 'application/json'},
+                    method: 'post',
+                    body: JSON.stringify(dado)
+                })
+                .then(res => this._handlerErros(res));
+
+        // return new Promise((resolve,reject) => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open("POST", url, true);
+        //     xhr.setRequestHeader("Content-Type","application/json");
+
+        //     xhr.onreadystatechange = () => {
+        //         if(xhr.readyState == 4){
+        //             if(xhr.status == 200){
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             }else{
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     };
+        //     xhr.send(JSON.stringify(dado));
+        // });
     }
 }
